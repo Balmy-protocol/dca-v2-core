eb50a7f8d0ef79e3182b8c634d610f973810f1a7
Alejo Amiras <alejo.amiras@gmail.com>
2021-04-22 22:15:55 -0300

    test: adds parallel testing to mocha (#27)

--- .mocharc.js
+++ .mocharc.js
@@ -3,6 +3,9 @@ dotenv.config();
 
 module.exports = {
   require: ['hardhat/register'],
+  extension: ['.ts'],
+  ignore: ['./test/utils/**'],
   recursive: true,
+  parallel: true,
   timeout: process.env.MOCHA_TIMEOUT || 300000,
 };
