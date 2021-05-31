793cd20b5aa484a3c6d0cac977a80b00ea9a4ef3
Alejo Amiras <alejo.amiras@gmail.com>
2021-05-30 21:58:43 -0300

    test: oracle util e2e

--- test/e2e/utils/oracle.spec.ts
+++ test/e2e/utils/oracle.spec.ts
@@ -1,16 +1,20 @@
+import { TransactionResponse } from '@ethersproject/abstract-provider';
 import { Contract, ContractFactory } from '@ethersproject/contracts';
-import { utils } from 'ethers';
+import { BigNumber, utils } from 'ethers';
 import { ethers } from 'hardhat';
-import { constants, evm } from '../../utils';
-import { then, when } from '../../utils/bdd';
+import { bn, constants, erc20, evm, wallet } from '../../utils';
+import { given, then, when } from '../../utils/bdd';
+import axios from 'axios';
+import { expect } from 'chai';
 
-const forkBlockNumber = 12529675;
+let forkBlockNumber: number;
 const uniswapFactoryAddress = '0x1F98431c8aD98523631AE4a59f267346ea31F984';
 
 describe('Oracle', () => {
   let uniswapFactory: Contract;
   let oracleFactory: ContractFactory;
   let oracle: Contract;
+  let cmcETHPrice: BigNumber;
 
   before(async () => {
     oracleFactory = await ethers.getContractFactory('contracts/utils/Oracle.sol:Oracle');
@@ -18,79 +22,82 @@ describe('Oracle', () => {
   });
 
   beforeEach(async () => {
-    await evm.reset({
-      jsonRpcUrl: process.env.MAINNET_HTTPS_URL,
-      blockNumber: forkBlockNumber,
-    });
+    if (!forkBlockNumber) {
+      const cmcReturn = await axios.get(
+        `https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=ETH&CMC_PRO_API_KEY=${process.env.COINMARKETCAP_API_KEY}&convert=USD`
+      );
+      cmcETHPrice = utils.parseEther(`${cmcReturn.data.data.ETH.quote.USD.price}`);
+      await evm.reset({
+        jsonRpcUrl: process.env.MAINNET_HTTPS_URL,
+      });
+      forkBlockNumber = (await ethers.provider.getBlockNumber()) - 6;
+    } else {
+      await evm.reset({
+        jsonRpcUrl: process.env.MAINNET_HTTPS_URL,
+        blockNumber: forkBlockNumber,
+      });
+    }
 
     oracle = await oracleFactory.deploy();
   });
 
   describe('getTwap', () => {
     when('pool is not a uniswap pool', () => {
-      then('call is reverted');
+      let getTWAPTx: Promise<TransactionResponse>;
+      given(async () => {
+        getTWAPTx = oracle.getTwap(await wallet.generateRandomAddress(), constants.NOT_ZERO_ADDRESS, 0, constants.NOT_ZERO_ADDRESS, 1);
+      });
+      then('call is reverted', async () => {
+        await expect(getTWAPTx).to.be.revertedWith('Transaction reverted: function call to a non-contract account');
+      });
     });
-    when('asking for ticks that pool does not have', () => {});
+    when('asking for ticks that pool does not have', () => {
+      let getTWAPTx: Promise<TransactionResponse>;
+      let poolAddress: string;
+      let tokenA: Contract;
+      let tokenB: Contract;
+      given(async () => {
+        tokenA = await erc20.deploy({
+          name: 'Token A',
+          symbol: 'TA',
+          initialAccount: await wallet.generateRandomAddress(),
+          initialAmount: utils.parseEther('1000'),
+        });
+        tokenB = await erc20.deploy({
+          name: 'Token B',
+          symbol: 'TB',
+          initialAccount: await wallet.generateRandomAddress(),
+          initialAmount: utils.parseEther('1000'),
+        });
+        poolAddress = await uniswapFactory.callStatic.createPool(tokenA.address, tokenB.address, 3000);
+        await uniswapFactory.createPool(tokenA.address, tokenB.address, 3000);
+        getTWAPTx = oracle.getTwap(poolAddress, tokenA.address, utils.parseEther('1'), tokenB.address, 60);
+      });
+      then('tx is reverted with reason', async () => {
+        // Reference: https://github.com/Uniswap/uniswap-v3-core/blob/main/contracts/libraries/Oracle.sol#L309
+        await expect(getTWAPTx).to.be.revertedWith('I');
+      });
+    });
     when('pool is valid and have valid ticks', () => {
+      let twapETHPrice: BigNumber;
+      const priceThreshold = utils.parseEther('15');
+      const period = 30;
+      given(async () => {
+        twapETHPrice = await oracle.getTwap(
+          '0xC2e9F25Be6257c210d7Adf0D4Cd6E3E881ba25f8', // dai-weth 0.3
+          '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', // weth
+          utils.parseEther('1'),
+          '0x6b175474e89094c44da98b954eedeac495271d0f', // dai
+          period
+        );
+      });
       then('returns twap', async () => {
-        console.log(
-          utils.formatEther(
-            await oracle.getTwap(
-              '0x60594a405d53811d3BC4766596EFD80fd545A270', // dai-weth 0.05%
-              '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', // weth
-              utils.parseEther('1'),
-              '0x6b175474e89094c44da98b954eedeac495271d0f', // dai
-              60 // seconds
-            )
-          )
-        );
-
-        console.log(
-          utils.formatEther(
-            await oracle.getTwap(
-              '0xC2e9F25Be6257c210d7Adf0D4Cd6E3E881ba25f8', // dai-weth 0.3
-              '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', // weth
-              utils.parseEther('1'),
-              '0x6b175474e89094c44da98b954eedeac495271d0f', // dai
-              60 // seconds
-            )
-          )
-        );
-
-        console.log(
-          utils.formatEther(
-            await oracle.getTwap(
-              '0xa80964C5bBd1A0E95777094420555fead1A26c1e', // dai-weth 1%
-              '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', // weth
-              utils.parseEther('1'),
-              '0x6b175474e89094c44da98b954eedeac495271d0f', // dai
-              60 // seconds
-            )
-          )
-        );
+        bn.expectToEqualWithThreshold({
+          value: twapETHPrice,
+          to: cmcETHPrice,
+          threshold: priceThreshold,
+        });
       });
     });
   });
 });
-
-// console.log('dai-weth 0.05%',
-//   await uniswapFactory.getPool(
-//     '0x6b175474e89094c44da98b954eedeac495271d0f', // dai
-//     '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', // weth
-//     500
-//   )
-// );
-// console.log('dai-weth 0.3%',
-//   await uniswapFactory.getPool(
-//     '0x6b175474e89094c44da98b954eedeac495271d0f', // dai
-//     '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', // weth
-//     3000
-//   )
-// );
-// console.log('dai-weth 1%',
-//   await uniswapFactory.getPool(
-//     '0x6b175474e89094c44da98b954eedeac495271d0f', // dai
-//     '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', // weth
-//     10000
-//   )
-// );
