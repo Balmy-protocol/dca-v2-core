f49bd3c7d145cf749814a76bea7aa9b48ed88296
Alejo Amiras <alejo.amiras@gmail.com>
2021-07-14 19:27:53 -0300

    chore: pr updates

--- test/integration/UniswapV3Oracle/quote.spec.ts
+++ test/integration/UniswapV3Oracle/quote.spec.ts
@@ -1,11 +1,11 @@
-import { BigNumber, Contract, utils } from 'ethers';
+import { Contract, utils } from 'ethers';
 import { deployments, ethers } from 'hardhat';
 import { getNodeUrl } from '../../../utils/network';
 import { evm } from '../../utils';
-import { contract, given } from '../../utils/bdd';
-import axios from 'axios';
+import { contract, given, then } from '../../utils/bdd';
 import moment from 'moment';
 import { expect } from 'chai';
+import { getLastPrice, convertPriceToNumberWithDecimals } from '../../utils/coingecko';
 
 let oracle: Contract;
 let startingTime: number;
@@ -15,6 +15,10 @@ let oraclePeriod: number = moment.duration('5', 'minutes').as('seconds');
 const PRICE_THRESHOLD = 15;
 const WETH = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
 const USDC = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48';
+
+const UNI_WETH_USDC_POOL_LOW = '0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640';
+const UNI_WETH_USDC_POOL_MEDIUM = '0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8';
+const UNI_WETH_USDC_POOL_HIGH = '0x7BeA39867e4169DBe237d55C8242a8f2fcDcc387';
 
 type CoingeckoDataPoints = {
   prices: [number, number][];
@@ -37,20 +41,16 @@ contract('UniswapV3Oracle', () => {
     let feedPrice: number;
     given(async () => {
       // Funny thing, coingecko updates this price feed every 5 minute (not a twap, but close enough).
-      const coingeckoDatapoints = (
-        await axios.get(
-          `https://api.coingecko.com/api/v3/coins/ethereum/market_chart/range?vs_currency=usd&from=${
-            startingTime - oraclePeriod
-          }&to=${startingTime}`
-        )
-      ).data as CoingeckoDataPoints;
-      feedPrice = coingeckoDatapoints.prices[0][1];
+      feedPrice = await getLastPrice('ethereum', 'usd', startingTime - oraclePeriod, startingTime);
     });
-    it('returns correct twap', async () => {
+    then('both USDC/WETH pools are used', async () => {
+      expect(await oracle.poolsUsedForPair(WETH, USDC)).to.eql([UNI_WETH_USDC_POOL_LOW, UNI_WETH_USDC_POOL_MEDIUM, UNI_WETH_USDC_POOL_HIGH]);
+    });
+    then('returns correct twap', async () => {
       const twap = await oracle.quote(WETH, utils.parseEther('1'), USDC);
       expect(twap).to.be.within(
-        utils.parseUnits(`${(feedPrice - PRICE_THRESHOLD).toFixed(6)}`, 6).toNumber(),
-        utils.parseUnits(`${(feedPrice + PRICE_THRESHOLD).toFixed(6)}`, 6).toNumber()
+        convertPriceToNumberWithDecimals(feedPrice - PRICE_THRESHOLD, 6),
+        convertPriceToNumberWithDecimals(feedPrice + PRICE_THRESHOLD, 6)
       );
     });
   });
---
+++ test/utils/coingecko.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { BigNumber, utils } from 'ethers';
+import moment from 'moment';
+
+type CoingeckoDataPoints = {
+  prices: [number, number][];
+  market_caps: [number, number][];
+  total_volumes: [number, number][];
+};
+
+export const getCoingeckoDataPoints = async (coin: string, currency: string, from: number, to: number): Promise<CoingeckoDataPoints> => {
+  const coingeckoDatapoints = (
+    await axios.get(`https://api.coingecko.com/api/v3/coins/${coin}/market_chart/range?vs_currency=${currency}&from=${from}&to=${to}`)
+  ).data as CoingeckoDataPoints;
+  return coingeckoDatapoints;
+};
+
+export const getLastPrice = async (coin: string, currency: string, from: number, to: number): Promise<number> => {
+  const coingeckoDataPoints = await getCoingeckoDataPoints(coin, currency, from, to);
+  return coingeckoDataPoints.prices[0][1];
+};
+
+export const convertPriceToBigNumberWithDecimals = (price: number, decimals: number): BigNumber => {
+  return utils.parseUnits(`${price.toFixed(6)}`, 6);
+};
+
+export const convertPriceToNumberWithDecimals = (price: number, decimals: number): number => {
+  return convertPriceToBigNumberWithDecimals(price, decimals).toNumber();
+};
