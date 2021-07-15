a73943a3a5af7ee333b1a8651e78619d56fab76d
Alejo Amiras <alejo.amiras@gmail.com>
2021-07-15 10:41:21 -0300

    test: integration for uniswap v3 quote (#109)

--- test/utils/index.ts
+++ test/utils/index.ts
@@ -5,5 +5,6 @@ import erc20 from './erc20';
 import evm from './evm';
 import bn from './bn';
 import wallet from './wallet';
+import * as coingecko from './coingecko';
 
-export { contracts, behaviours, bn, constants, erc20, evm, wallet };
+export { contracts, coingecko, behaviours, bn, constants, erc20, evm, wallet };
