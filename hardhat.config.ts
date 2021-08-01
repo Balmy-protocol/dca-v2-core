d2cd5ac0fd898629c122f94156cb4c3ebce2f7ab
Alejo Amiras <alejo.amiras@gmail.com>
2021-08-01 17:15:11 -0300

    refactor: rename swapper to dca uniswap v3 swapper deployment (#159)

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,6 +9,7 @@ import 'hardhat-deploy';
 import 'solidity-coverage';
 import { HardhatUserConfig, NetworksUserConfig } from 'hardhat/types';
 import { getNodeUrl, accounts } from './utils/network';
+import { utils } from 'ethers';
 
 const networks: NetworksUserConfig = process.env.TEST
   ? {}
@@ -35,7 +36,7 @@ const networks: NetworksUserConfig = process.env.TEST
       rinkeby: {
         url: getNodeUrl('rinkeby'),
         accounts: accounts('rinkeby'),
-        gasPrice: 'auto',
+        gasPrice: utils.parseUnits('1.5', 'gwei').toNumber(),
         tags: ['staging'],
       },
       mainnet: {
--- scripts/add-watched-pairs.ts
+++ scripts/add-watched-pairs.ts
@@ -3,7 +3,7 @@ import { run, ethers } from 'hardhat';
 async function main() {
   run('compile');
   const [, governor] = await ethers.getSigners();
-  const DCASwapper = await ethers.getContract('Swapper', governor);
+  const DCASwapper = await ethers.getContract('DCAUniswapV3Swapper', governor);
   const tx = await DCASwapper.startWatchingPairs(['0x81d2b6296352765e4d73be089e1dd8499fc0da14', '0xa35432c76c6df79376ba5bcefe8c344661cdeef1']);
   console.log('Start watching pairs tx', tx.hash);
 }
