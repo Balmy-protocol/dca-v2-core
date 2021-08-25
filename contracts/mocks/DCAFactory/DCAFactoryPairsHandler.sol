3162f849916e33f78aebccbda4f108fd6dd3ad7a
Nicolas Chamo <nicolas@chamo.com.ar>
2021-08-25 11:14:32 -0300

    refactor: delete factory

--- contracts/mocks/DCAFactory/DCAFactoryPairsHandler.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-
-pragma solidity ^0.8.6;
-
-import '../../DCAFactory/DCAFactoryPairsHandler.sol';
-
-contract DCAFactoryPairsHandlerMock is DCAFactoryPairsHandler {
-  constructor(IDCAGlobalParameters _globalParameters) DCAFactoryPairsHandler(_globalParameters) {}
-}
