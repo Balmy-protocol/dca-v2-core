096055d7e7ca88cca41bbc86806245b2ec7b9cb9
Nicolas Chamo <nicolas@chamo.com.ar>
2021-08-25 19:46:55 -0300

    refactor: delete factory (#12)

--- contracts/DCAFactory/DCAFactory.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: BUSL-1.1
-pragma solidity ^0.8.6;
-
-import './DCAFactoryPairsHandler.sol';
-
-contract DCAFactory is DCAFactoryPairsHandler, IDCAFactory {
-  constructor(IDCAGlobalParameters _globalParameters) DCAFactoryPairsHandler(_globalParameters) {}
-}
--- contracts/mocks/DCAFactory/DCAFactory.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.6;
-
-import './DCAFactoryPairsHandler.sol';
-
-contract DCAFactoryMock is DCAFactoryPairsHandlerMock, IDCAFactory {
-  constructor(IDCAGlobalParameters _globalParameters) DCAFactoryPairsHandlerMock(_globalParameters) {}
-}
