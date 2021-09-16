d31e24c8611b6a1f0ea9a1f723840e710a860561
Nicolas Chamo <nicolas@chamo.com.ar>
2021-09-16 19:17:25 -0300

    feat: add mint

--- contracts/DCAPermissionsManager/DCAPermissionsManager.sol
+++ contracts/DCAPermissionsManager/DCAPermissionsManager.sol
@@ -1,16 +1,80 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.6;
 
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+
+enum Permission {
+  INCREASE,
+  REDUCE,
+  WITHDRAW,
+  TERMINATE
+}
+
+library PermissionMath {
+  function toUInt8(Permission[] memory _permissions) internal pure returns (uint8 _representation) {
+    for (uint256 i; i < _permissions.length; i++) {
+      _representation += uint8(2**uint8(_permissions[i]));
+    }
+  }
+
+  function hasPermission(uint8 _representation, Permission _permission) internal pure returns (bool) {
+    uint256 _bitMask = 2**uint256(_permission);
+    return (_representation & _bitMask) == _bitMask;
+  }
+}
+
 // Note: ideally, this would be part of the DCAHub. However, since we've reached the max bytecode size, we needed to make it its own contract
-contract DCAPermissionsManager {
+contract DCAPermissionsManager is ERC721 {
   error HubAlreadySet();
   error ZeroAddress();
+  error IdAlreadyInUse();
+  error OnlyHubCanMint();
+
+  struct PermissionSet {
+    address operator;
+    Permission[] permissions;
+  }
+
+  event Minted(uint256 id, address owner, PermissionSet[] permissions);
+
+  using PermissionMath for Permission[];
+  using PermissionMath for uint8;
 
   address public hub;
+  mapping(uint256 => mapping(address => uint8)) internal _operators;
+
+  constructor() ERC721('Mean Finance DCA', 'DCA') {}
 
   function setHub(address _hub) external {
     if (_hub == address(0)) revert ZeroAddress();
     if (hub != address(0)) revert HubAlreadySet();
     hub = _hub;
+  }
+
+  function mint(
+    uint256 _id,
+    address _owner,
+    PermissionSet[] calldata _permissions
+  ) external {
+    if (msg.sender != hub) revert OnlyHubCanMint();
+    _mint(_owner, _id);
+    _setPermissions(_id, _permissions);
+
+    emit Minted(_id, _owner, _permissions);
+  }
+
+  function hasPermission(
+    uint256 _id,
+    address _address,
+    Permission _permission
+  ) external view returns (bool) {
+    return ownerOf(_id) == _address || _operators[_id][_address].hasPermission(_permission);
+  }
+
+  function _setPermissions(uint256 _id, PermissionSet[] calldata _permissions) internal {
+    for (uint256 i; i < _permissions.length; i++) {
+      if (_permissions[i].operator == address(0)) revert ZeroAddress();
+      _operators[_id][_permissions[i].operator] = _permissions[i].permissions.toUInt8();
+    }
   }
 }
--- test/unit/DCAPermissionsManager/dca-permissions-manager.spec.ts
+++ test/unit/DCAPermissionsManager/dca-permissions-manager.spec.ts
@@ -5,19 +5,23 @@ import { constants, wallet, behaviours } from '@test-utils';
 import { given, then, when, contract } from '@test-utils/bdd';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signers';
 import { snapshot } from '@test-utils/evm';
+import { Permission } from 'js-lib/types';
+import { TransactionResponse } from '@ethersproject/abstract-provider';
+import { readArgFromEventOrFail } from '@test-utils/event-utils';
 
-contract('DCAPermissionsManager', () => {
-  let owner: SignerWithAddress;
+contract.only('DCAPermissionsManager', () => {
+  let hub: SignerWithAddress;
   let DCAPermissionsManagerFactory: DCAPermissionsManager__factory;
   let DCAPermissionsManager: DCAPermissionsManager;
   let snapshotId: string;
 
   before('Setup accounts and contracts', async () => {
-    [owner] = await ethers.getSigners();
+    [hub] = await ethers.getSigners();
     DCAPermissionsManagerFactory = await ethers.getContractFactory(
       'contracts/DCAPermissionsManager/DCAPermissionsManager.sol:DCAPermissionsManager'
     );
     DCAPermissionsManager = await DCAPermissionsManagerFactory.deploy();
+    await DCAPermissionsManager.setHub(hub.address);
     snapshotId = await snapshot.take();
   });
 
@@ -26,10 +30,22 @@ contract('DCAPermissionsManager', () => {
   });
 
   describe('constructor', () => {
+    let DCAPermissionsManager: DCAPermissionsManager;
+    given(async () => {
+      DCAPermissionsManager = await DCAPermissionsManagerFactory.deploy();
+    });
     when('manager is deployed', () => {
       then('hub is zero address', async () => {
         const hub = await DCAPermissionsManager.hub();
         expect(hub).to.equal(constants.ZERO_ADDRESS);
+      });
+      then('name is correct', async () => {
+        const name = await DCAPermissionsManager.name();
+        expect(name).to.equal('Mean Finance DCA');
+      });
+      then('symbol is correct', async () => {
+        const symbol = await DCAPermissionsManager.symbol();
+        expect(symbol).to.equal('DCA');
       });
     });
   });
@@ -48,7 +64,9 @@ contract('DCAPermissionsManager', () => {
 
     when('parameter is a valid address', () => {
       const ADDRESS = wallet.generateRandomAddress();
+      let DCAPermissionsManager: DCAPermissionsManager;
       given(async () => {
+        DCAPermissionsManager = await DCAPermissionsManagerFactory.deploy();
         await DCAPermissionsManager.setHub(ADDRESS);
       });
       then('hub is set correctly', async () => {
@@ -58,9 +76,6 @@ contract('DCAPermissionsManager', () => {
     });
 
     when('hub is already set', () => {
-      given(async () => {
-        await DCAPermissionsManager.setHub(wallet.generateRandomAddress());
-      });
       then('reverts with message', async () => {
         await behaviours.txShouldRevertWithMessage({
           contract: DCAPermissionsManager,
@@ -68,6 +83,80 @@ contract('DCAPermissionsManager', () => {
           args: [constants.NOT_ZERO_ADDRESS],
           message: 'HubAlreadySet',
         });
+      });
+    });
+  });
+
+  describe('mint', () => {
+    const TOKEN_ID = 1;
+    when('owner is zero address', () => {
+      then('reverts with message', async () => {
+        await behaviours.txShouldRevertWithMessage({
+          contract: DCAPermissionsManager,
+          func: 'mint',
+          args: [1, constants.ZERO_ADDRESS, []],
+          message: 'ERC721: mint to the zero address',
+        });
+      });
+    });
+    when('id is already in use', () => {
+      given(async () => {
+        await DCAPermissionsManager.mint(TOKEN_ID, constants.NOT_ZERO_ADDRESS, []);
+      });
+      then('reverts with message', async () => {
+        await behaviours.txShouldRevertWithMessage({
+          contract: DCAPermissionsManager,
+          func: 'mint',
+          args: [TOKEN_ID, constants.NOT_ZERO_ADDRESS, []],
+          message: 'ERC721: token already minted',
+        });
+      });
+    });
+    when('caller is not the hub', () => {
+      then('reverts with message', async () => {
+        await behaviours.txShouldRevertWithMessage({
+          contract: DCAPermissionsManager.connect(await wallet.generateRandom()),
+          func: 'mint',
+          args: [TOKEN_ID, constants.NOT_ZERO_ADDRESS, []],
+          message: 'OnlyHubCanMint',
+        });
+      });
+    });
+    when('mint is executed', () => {
+      const OWNER = wallet.generateRandomAddress();
+      const OPERATOR = constants.NOT_ZERO_ADDRESS;
+      let tx: TransactionResponse;
+
+      given(async () => {
+        tx = await DCAPermissionsManager.mint(TOKEN_ID, OWNER, [{ operator: OPERATOR, permissions: [Permission.WITHDRAW] }]);
+      });
+
+      then('permissions are assigned properly', async () => {
+        expect(await DCAPermissionsManager.hasPermission(TOKEN_ID, OPERATOR, Permission.WITHDRAW)).to.be.true;
+      });
+
+      then('no extra permissions are assigned', async () => {
+        for (const permission of [Permission.INCREASE, Permission.REDUCE, Permission.TERMINATE]) {
+          expect(await DCAPermissionsManager.hasPermission(TOKEN_ID, OPERATOR, permission)).to.be.false;
+        }
+      });
+
+      then('nft is created and assigned to owner', async () => {
+        const tokenOwner = await DCAPermissionsManager.ownerOf(TOKEN_ID);
+        const balance = await DCAPermissionsManager.balanceOf(OWNER);
+        expect(tokenOwner).to.equal(OWNER);
+        expect(balance).to.equal(TOKEN_ID);
+      });
+
+      then('event is emitted', async () => {
+        const id = await readArgFromEventOrFail(tx, 'Minted', 'id');
+        const owner = await readArgFromEventOrFail(tx, 'Minted', 'owner');
+        const permissions: any = await readArgFromEventOrFail(tx, 'Minted', 'permissions');
+        expect(id).to.equal(TOKEN_ID);
+        expect(owner).to.equal(OWNER);
+        expect(permissions.length).to.equal(1);
+        expect(permissions[0].operator).to.equal(OPERATOR);
+        expect(permissions[0].permissions).to.eql([Permission.WITHDRAW]);
       });
     });
   });
