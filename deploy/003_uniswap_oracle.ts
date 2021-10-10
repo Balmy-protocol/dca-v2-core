cc4dc81d754633d174ffd9362def4bdd8742c2e0
Nicolas Chamo <nicolas@chamo.com.ar>
2021-10-10 12:54:39 -0300

    fix: fix integration tests

--- deploy/003_uniswap_oracle.ts
+++ deploy/003_uniswap_oracle.ts
@@ -7,7 +7,7 @@ const deployFunction: DeployFunction = async function (hre: HardhatRuntimeEnviro
   const UNISWAP_V3_FACTORY_ADDRESS = '0x1F98431c8aD98523631AE4a59f267346ea31F984';
 
   await hre.deployments.deploy('UniswapOracle', {
-    contract: 'contracts/UniswapV3Oracle/UniswapV3Oracle.sol:UniswapV3Oracle',
+    contract: 'contracts/oracles/UniswapV3Oracle.sol:UniswapV3Oracle',
     from: deployer,
     args: [governor, UNISWAP_V3_FACTORY_ADDRESS],
     log: true,
