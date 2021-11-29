391e27e2d356257fdc7f11bb5c45a2a54365ea0f
Nicolas Chamo <nicolas@chamo.com.ar>
2021-11-29 16:23:01 -0300

    chore: build verification script

--- scripts/verify-contracts.ts
+++ scripts/verify-contracts.ts
@@ -2,59 +2,47 @@ import { deployments } from 'hardhat';
 import { run } from 'hardhat';
 
 async function main() {
-  console.time('Verifying token descriptor ...');
-  const tokenDescriptor = await deployments.getOrNull('TokenDescriptor');
-  await run('verify:verify', {
-    address: tokenDescriptor!.address,
-    constructorArguments: tokenDescriptor!.args,
-    contract: 'contracts/DCATokenDescriptor/DCATokenDescriptor.sol:DCATokenDescriptor',
+  await verify({
+    name: 'ChainlinkOracle',
+    path: 'contracts/oracles/ChainlinkOracle.sol:ChainlinkOracle',
   });
-  console.timeEnd('Verifying token descriptor ...');
 
-  console.time('Verifying uniswap oracle ...');
-  const uniswapOracle = await deployments.getOrNull('UniswapOracle');
-  await run('verify:verify', {
-    address: uniswapOracle!.address,
-    constructorArguments: uniswapOracle!.args,
-    contract: 'contracts/UniswapV3Oracle/UniswapV3Oracle.sol:UniswapV3Oracle',
+  await verify({
+    name: 'UniswapOracle',
+    path: 'contracts/oracles/UniswapV3Oracle.sol:UniswapV3Oracle',
   });
-  console.timeEnd('Verifying uniswap oracle ...');
 
-  console.time('Verifying global parameters ...');
-  const globalParameters = await deployments.getOrNull('GlobalParameters');
-  await run('verify:verify', {
-    address: globalParameters!.address,
-    constructorArguments: globalParameters!.args,
-    contract: 'contracts/DCAGlobalParameters/DCAGlobalParameters.sol:DCAGlobalParameters',
+  await verify({
+    name: 'OracleAggregator',
+    path: 'contracts/oracles/OracleAggregator.sol:OracleAggregator',
   });
-  console.timeEnd('Verifying global parameters ...');
 
-  console.time('Verifying factory ...');
-  const factory = await deployments.getOrNull('Factory');
-  await run('verify:verify', {
-    address: factory!.address,
-    constructorArguments: factory!.args,
-    contract: 'contracts/DCAFactory/DCAFactory.sol:DCAFactory',
+  await verify({
+    name: 'TokenDescriptor',
+    path: 'contracts/DCATokenDescriptor/DCATokenDescriptor.sol:DCATokenDescriptor',
   });
-  console.timeEnd('Verifying factory ...');
 
-  console.time('Verifying dca uniswap v3 swapper ...');
-  const swapper = await deployments.getOrNull('DCAUniswapV3Swapper');
-  await run('verify:verify', {
-    address: swapper!.address,
-    constructorArguments: swapper!.args,
-    contract: 'contracts/DCASwapper/DCAUniswapV3Swapper.sol:DCAUniswapV3Swapper',
+  await verify({
+    name: 'PermissionsManager',
+    path: 'contracts/DCAPermissionsManager/DCAPermissionsManager.sol:DCAPermissionsManager',
   });
-  console.timeEnd('Verifying dca uniswap v3 swapper ...');
 
-  console.time('Verifying keep3r job ...');
-  const keep3rJob = await deployments.getOrNull('Keep3rJob');
-  await run('verify:verify', {
-    address: keep3rJob!.address,
-    constructorArguments: keep3rJob!.args,
-    contract: 'contracts/DCAKeep3rJob/DCAKeep3rJob.sol:DCAKeep3rJob',
+  await verify({
+    name: 'DCAHub',
+    path: 'contracts/DCAHub/DCAHub.sol:DCAHub',
   });
-  console.timeEnd('Verifying keep3r job ...');
+}
+
+async function verify({ name, path }: { name: string; path: string }) {
+  const title = `Verified ${name} ...`;
+  console.time(title);
+  const contract = await deployments.getOrNull(name);
+  await run('verify:verify', {
+    address: contract!.address,
+    constructorArguments: contract!.args,
+    contract: path,
+  });
+  console.timeEnd(title);
 }
 
 main()
