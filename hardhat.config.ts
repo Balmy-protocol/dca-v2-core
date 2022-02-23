f81c84a0c4a42ac76dddc54c27ea1c6525110e50
Ged <0xged@protonmail.com>
2022-02-23 10:14:45 -0300

    feat: adds optimization for uniswap library

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -110,12 +110,30 @@ const config: HardhatUserConfig = {
     overrides: {
       '@uniswap/v3-core/contracts/libraries/FullMath.sol': {
         version: '0.7.6',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
       },
       '@uniswap/v3-core/contracts/libraries/TickMath.sol': {
         version: '0.7.6',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
       },
       '@uniswap/v3-periphery/contracts/libraries/PoolAddress.sol': {
         version: '0.7.6',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
       },
     },
   },
