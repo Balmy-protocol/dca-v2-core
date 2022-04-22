3e1446c84230c73e925a194135e88e38d39c8b78
Ged <97693615+0xged@users.noreply.github.com>
2022-04-22 14:54:58 -0300

    feat: update solidity version (#185)
    * perf: optimizes optimizer for hub and descriptor (#188)

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -95,7 +95,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: '0.8.10',
+        version: '0.8.13',
         settings: {
           optimizer: {
             enabled: true,
@@ -112,17 +112,26 @@ const config: HardhatUserConfig = {
           },
         },
       },
-      {
-        version: '0.8.3',
+    ],
+    overrides: {
+      'contracts/DCAHub/DCAHub.sol': {
+        version: '0.8.13',
         settings: {
           optimizer: {
             enabled: true,
-            runs: 200,
+            runs: 830,
           },
         },
       },
-    ],
-    overrides: {
+      'contracts/DCATokenDescriptor/DCATokenDescriptor.sol': {
+        version: '0.8.13',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 0,
+          },
+        },
+      },
       '@uniswap/v3-core/contracts/libraries/FullMath.sol': {
         version: '0.7.6',
       },
